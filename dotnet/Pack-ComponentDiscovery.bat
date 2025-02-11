946fbd65f14adaca3830cd318ae4e2bfbc4e1ed1
T. Korn <tobias.korn@towerswatson.com>
2025-02-11 08:33:24 +0100

    fixed paths

--- dotnet/Pack-ComponentDiscovery.bat
+++
@@ -1,6 +0,0 @@
-nuget pack ./ComponentDiscovery.nuspec -Build -Symbols -OutputDirectory ".\(Stage)\Packages" -InstallPackageToOutputPath
-IF NOT EXIST "..\..\(NuGetRepo)" GOTO NOCOPYTOGLOBALREPO
-xcopy ".\dist\Packages\*.nuspec" "..\..\(NuGetRepo)\" /d /r /y /s
-xcopy ".\dist\Packages\*.nupkg*" "..\..\(NuGetRepo)\" /d /r /y /s
-:NOCOPYTOGLOBALREPO
-PAUSE
